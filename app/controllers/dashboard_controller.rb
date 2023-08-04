83c3739aa3e769be2f3468cfe06a19d5bb98b9e2
ochoj <jonatanch27@gmail.com>
2023-08-03 19:16:43 -0600

    add funtion of dasdhboard tabs

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class DashboardController < ApplicationController
+  #constants
+  DEVICES = 'devices'
+  PERIPHERALS = 'peripherals'
+
   def index
-    @devices = Device.all
-    @peripherals = Peripheral.all
+    if tab_param == DEVICES or params[:tab] == nil
+      @devices = Device.all
+    else 
+      @peripherals = Peripheral.all
+    end
+  end
+  def tab_param
+    @tab_param = params[:tab]
   end
 end
